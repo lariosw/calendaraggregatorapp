a5842e0bae5ad5581bae5887d13c89257c303350
lariosw <lariosw@seattleu.edu>
2016-12-01 19:05:45 -0800

    additional changes

--- app/scripts/services/citysearch.js
+++ app/scripts/services/citysearch.js
@@ -13,7 +13,7 @@ angular.module('calendaraggregatorappApp')
     // ...
 
   // Public API here
-    return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&mode=json&APPID=4ef70d1fed3e3e197bc3ec9cee13e2b2', {}, {
+    return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&mode=json&units=imperial&APPID=4ef70d1fed3e3e197bc3ec9cee13e2b2', {}, {
       find: {
         method:'GET',
         params:{
